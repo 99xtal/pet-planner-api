b2f9f1bbe7ae8f0bb7d795cc9b9798c10e5ed868
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-05-19 16:05:59 -0400

    PPA-13 Create medications app + Create Medicine, Medication models + Add models to admin site + Add medications app to project

---
+++ medications/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from pets.models import PetCategory
+
+# Create your models here.
+class Medicine(models.Model):
+    name = models.CharField(max_length=64)
+    pet_category = models.ForeignKey(PetCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.pet_category__category})"
+
+class Medication(models.Model):
+    UNIT_CHOICES = [
+        ('pills','pills'),
+        ('ccs', 'ccs'),
+        ('drops', 'drops')
+    ]
+    medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount_units = models.CharField(max_length=10, choices=UNIT_CHOICES)
+    time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.amount} {self.amount_units} {self.medicine}, {self.time}"
+    
