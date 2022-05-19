bcd6a2c7f96d19ff6c839a989e8742dbf58fdc9b
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-05-19 14:27:57 -0400

    PPA-12 Create meals app + Create Food, Meal models + Add models to admin site + Add many-to-many field "meals" to Pet model + Add meals app to project

--- pets/serializers.py
+++ pets/serializers.py
@@ -5,8 +5,10 @@ class PetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pet
         fields = ['id', 'user', 'name', 'category', 'breed', 'birthday', 'gender', 'weight']
+        depth = 1
 
 class BreedSerializer(serializers.ModelSerializer):
     class Meta:
         model = Breed
         fields = '__all__'
+        depth = 1
