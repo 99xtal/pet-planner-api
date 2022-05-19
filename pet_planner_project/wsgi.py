b13a41c99d6585d25a2e003dc7d0807b89db721a
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-05-19 10:52:28 -0400

    PPA-9 Finish Django project configuration + Set up database and run migrations + Remove apps from apps folder

--- pet_planner_project/wsgi.py
+++ pet_planner_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for drf_jwt_backend project.
+WSGI config for pet_planner_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'drf_jwt_backend.settings')
+                      'pet_planner_project.settings')
 
 application = get_wsgi_application()
