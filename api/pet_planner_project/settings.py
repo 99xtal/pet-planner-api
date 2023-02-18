bc45e4b265dc1195c73d952668fb77ddc1894e46
Joe Rybarczyk <joerybarczyk99@gmail.com>
2023-02-17 21:13:15 -0500

    feat(cd): Add docker compose deployment step - ALLOWED_HOSTS fix

--- api/pet_planner_project/settings.py
+++ api/pet_planner_project/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "INSECURE")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "amazonaws.com"
+]
 
 AUTH_USER_MODEL = "authentication.User"
 
