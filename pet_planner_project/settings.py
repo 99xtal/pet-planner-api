ff9ac5c53581bd46215c0695869fa9b45e426702
Joe Rybarczyk <joe.rybarczyk@me.com>
2023-04-21 22:22:58 -0400

    configure EB

--- pet_planner_project/settings.py
+++ pet_planner_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "INSECURE")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["django-api-dev.us-east-1.elasticbeanstalk.com"]
 
 AUTH_USER_MODEL = "authentication.User"
 
